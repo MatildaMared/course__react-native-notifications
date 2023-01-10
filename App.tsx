f478965335b9fa85fc0d08b14897f6dc45eb81d4
Matilda Mared <matildamared@live.se>
2023-01-10 08:28:46 +0100

    test notifications

--- App.tsx
+++ App.tsx
@@ -1,20 +1,99 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View, Platform } from "react-native";
+import * as Notifications from "expo-notifications";
+import { useEffect, useState } from "react";
+
+Notifications.setNotificationHandler({
+	handleNotification: async () => {
+		console.log("Notification received");
+		return {
+			shouldShowAlert: true,
+			shouldPlaySound: true,
+			shouldSetBadge: true,
+		};
+	},
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [token, setToken] = useState<string>();
+
+	function scheduleNotificationHandler() {
+		Notifications.scheduleNotificationAsync({
+			content: {
+				title: "My first local notification",
+				body: "This is the first local notification we are sending!",
+				data: {
+					username: "Matilda",
+				},
+			},
+			trigger: {
+				seconds: 5,
+			},
+		});
+	}
+
+	async function registerForPushNotificationsAsync() {
+		const { status: existingStatus } =
+			await Notifications.getPermissionsAsync();
+		let finalStatus = existingStatus;
+		if (existingStatus !== "granted") {
+			const { status } = await Notifications.requestPermissionsAsync();
+			finalStatus = status;
+		}
+		if (finalStatus !== "granted") {
+			alert("Failed to get push token for push notification!");
+			return;
+		}
+		// const token = (await Notifications.getExpoPushTokenAsync()).data;
+		// console.log(token);
+		// setToken(token);
+
+		if (Platform.OS === "android") {
+			Notifications.setNotificationChannelAsync("default", {
+				name: "default",
+				importance: Notifications.AndroidImportance.MAX,
+				vibrationPattern: [0, 250, 250, 250],
+				lightColor: "#FF231F7C",
+			});
+		}
+	}
+
+	useEffect(() => {
+		registerForPushNotificationsAsync();
+		const receivedSubscription = Notifications.addNotificationReceivedListener(
+			(notification) => {
+				console.log(notification.request.content.data.username);
+			}
+		);
+
+		const responseSubscription =
+			Notifications.addNotificationResponseReceivedListener((response) => {
+				console.log(response);
+			});
+
+		return () => {
+			receivedSubscription.remove();
+			responseSubscription.remove();
+		};
+	}, []);
+
+	return (
+		<View style={styles.container}>
+			<Button
+				title="Schedule Notification"
+				onPress={scheduleNotificationHandler}
+			/>
+			<Text>Open up App.tsx to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
